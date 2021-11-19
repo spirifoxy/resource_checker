6bf491290b72d8930535934b1f46e5e510197000
spirifoxy <spirifoxy@gmail.com>
2021-11-19 03:16:32 +0100

    Null-safety

--- lib/resource_checker.dart
+++ lib/resource_checker.dart
@@ -5,18 +5,18 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 
 class ResourceChecker {
-  static ResourceChecker _instance;
-  final Map<String, Object> _assetsMap;
+  static ResourceChecker? _instance;
+  late final Map<String, Object> _assetsMap;
 
   ResourceChecker._(this._assetsMap);
 
   static Future<ResourceChecker> _getInstance() async {
     if (_instance == null) {
       final allAssets = await rootBundle.loadString('AssetManifest.json');
-      final assetsMap = json.decode(allAssets) as Map<String, dynamic>;
+      final assetsMap = json.decode(allAssets) as Map<String, Object>;
       _instance = ResourceChecker._(assetsMap);
     }
-    return _instance;
+    return _instance!;
   }
 
   static Future<ResourceChecker> get instance async => await _getInstance();
